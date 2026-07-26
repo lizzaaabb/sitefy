635f9cd800e94e1ffe57c2d3f0639dad50c1dfda
lizzaaabb <lbogveradze12@gmail.com>
2026-07-27 00:28:24 +0400

    dark

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -4,12 +4,30 @@ import '../globals.css'
 import ThemeInitializer from '../../components/theme/ThemeInitializer'
 import Header from '../../components/home/Header'
 
+const themeScript = `
+  (function() {
+    try {
+      var formatter = new Intl.DateTimeFormat('en-US', {
+        timeZone: 'Asia/Tbilisi',
+        hour: 'numeric',
+        hour12: false,
+      });
+      var hour = parseInt(formatter.format(new Date()), 10);
+      var dark = hour >= 21 || hour < 7;
+      document.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light');
+    } catch (e) {}
+  })();
+`
+
 export default async function RootLayout({ children, params }) {
   const { locale } = await params
   const messages = await getMessages()
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+      </head>
       <body>
         <NextIntlClientProvider messages={messages}>
           <ThemeInitializer />
